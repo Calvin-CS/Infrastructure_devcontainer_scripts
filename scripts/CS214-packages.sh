25d8440627111178646527f868ed43870fea7b57
Chris Wieringa <cwieri39@calvin.edu>
2026-08-14 15:53:33 -0400

    add gh cli for eric

--- scripts/CS214-packages.sh
+++ scripts/CS214-packages.sh
@@ -41,3 +41,21 @@ apt-add-repository -y ppa:swi-prolog/stable && \
 
 # gnat-14 gnatmake update-alternatives
 update-alternatives --install /usr/bin/gnatmake gnatmake /usr/bin/gnatmake-14 100
+
+## gh cli
+# https://github.com/cli/cli/blob/trunk/docs/install_linux.md#debian
+sudo mkdir -p -m 755 /etc/apt/keyrings
+mkdir -p -m 755 /etc/apt/sources.list.d
+# only download the keyring if it doesn't already exist
+if [ ! -f /etc/apt/keyrings/githubcli-archive-keyring.gpg ]; then
+    out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg
+    cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null
+    rm -f $out
+fi
+chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg
+# only create the sources list if it doesn't already exist
+if [ ! -f /etc/apt/sources.list.d/github-cli.list ]; then
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+fi
+sudo apt update
+sudo apt install gh -y
